f2c064324b9467af45016440d18ad35c4ee46a21
Russell Smith <russell@octopart.com>
2018-11-20 03:45:43 -0500

    Use mplayer and organize barcodeplay code

--- barcode/barcodeplay.py
+++ barcode/barcodeplay.py
@@ -1,57 +1,101 @@
+import time
+import logging
+import argparse
+
 from imutils.video import VideoStream
 from pyzbar import pyzbar
-import imutils
-import cv2
-import time
-
 import gmusicplay
 
+
+log = logging
 QQQR_ROOT_URI = 'qqqr:gmusic:track:'
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
 
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
+def init():
+    log.debug('init')
+    t0 = time.time()
 
-print("[INFO] starting loop...")
+    # Initialize video stream.
+    vs = VideoStream(usePiCamera=True).start()
 
-# loop over the frames from the video stream
-i = 0
-while True:
-    start = time.time()
-    # grab the frame from the threaded video stream and resize it to
-    # have a maximum width of 400 pixels
-    frame = vs.read()
-    #frame = imutils.resize(frame, width=400, inter=cv2.INTER_CUBIC)
-    #cv2.imwrite('%00d.jpg' % i, frame)
-    i += 1
- 
-    # find the barcodes in the frame and decode each of the barcodes
-    barcodes = pyzbar.decode(frame)
-    end = time.time()
+    # Initialize Google Play Music api.
+    gmusicplay.init_gmusic_api()
 
-    print("[INFO] new frame in %0.2fs" % (end-start))
+    # Ensure camera sensor has had time to warm up.
+    while time.time() - t0 < 2.0:
+        time.sleep(0.1)
 
-    if barcodes:
-        break
+    return vs
 
 
-if barcodes:
-    barcode = barcodes[0]
-    barcodeData = barcode.data.decode("utf-8")
-    barcodeType = barcode.type
-    print('found: %s: %s' % (barcodeType, barcodeData))
-
-    if not barcodeData.startswith(QQQR_ROOT_URI):
-        raise Exception('Unexpected QR data: %s' % barcodeData)
-
-    track_id = barcodeData[len(QQQR_ROOT_URI):]
-
-    gmusicplay.play_gmusic_track_blocking(track_id)
-
-    #time.sleep(0.3)
+def cleanup(vs):
+    log.debug('cleanup')
+    vs.stop()
 
 
-print("[INFO] cleaning up...")
-vs.stop()
+def scan_barcodes_loop(vs, tmax=120):
+    log.debug('scan_barcodes_loop')
+    log.info('Ready for barcodes...')
+    t0 = time.time()
+
+    while True:
+        tf = time.time()
+        if tf - t0 > tmax:
+            return None
+
+        # Get a frame of video.
+        frame = vs.read()
+
+        # Detect barcodes in the frame.
+        barcodes = pyzbar.decode(frame)
+
+        log.debug("Scanned new frame in %0.2fs" % (time.time() - tf))
+
+        if barcodes:
+            return barcodes
+
+
+def parse_qqqr_barcode(barcode):
+    log.info('parse_qqqr_barcode')
+    if barcode.type != 'QRCODE':
+        raise Exception('Unexpected barcode type: %s' % barcode.type)
+
+    data = barcode.data.decode('utf-8')
+    log.info('Found barcode: %s: %s' % (barcode.type, data))
+
+    if not data.startswith(QQQR_ROOT_URI):
+        raise Exception('Unexpected QR data: %s' % data)
+
+    track_id = data[len(QQQR_ROOT_URI):]
+    return track_id
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action="store_true", default=False)
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    vs = init()
+
+    while True:
+        barcodes = scan_barcodes_loop(vs, tmax=120)
+        if not barcodes:
+            # Timed out without detecting a barcode.
+            break
+
+        if len(barcodes) > 1:
+            log.warning('detected multiple barcodes in frame')
+
+        track_id = parse_qqqr_barcode(barcodes[0])
+        gmusicplay.stream_gmusic_track_blocking(track_id)
+
+    cleanup(vs)
+
+
+if __name__ == '__main__':
+    main()
--- barcode/gmusicplay.py
+++ barcode/gmusicplay.py
@@ -1,6 +1,7 @@
+import time
 import argparse
-import requests
-import tempfile
+#import requests
+#import tempfile
 import subprocess
 
 from gmusicapi import Mobileclient
@@ -11,43 +12,70 @@ api = None
 
 
 def init_gmusic_api():
+    t0 = time.time()
     print('init_gmusic_api')
     global api
     api = Mobileclient()
     logged_in = api.login(gmusic_secrets.USERNAME, gmusic_secrets.PW, Mobileclient.FROM_MAC_ADDRESS)
     assert(logged_in)
+    print('init_gmusic_api finished: %0.2f' % (time.time() - t0))
 
 
-def play_mp3_file_blocking(f):
-    print('play_mp3_file_blocking')
-    subprocess.call(['mpg321', '-q', '-a', 'bluealsa', f.name])
+#def play_mp3_file_blocking(f):
+#    print('play_mp3_file_blocking')
+#    subprocess.call(['mpg321', '-q', '-a', 'bluealsa', f.name])
 
 
-def play_gmusic_track_blocking(track_id, verbose=False):
+def stream_mp3_url_blocking(url):
+    print('stream_mp3_url_blocking')
+    subprocess.call(['mplayer', '-cache', '20000', '-msgmodule', '-msglevel', 'global=0:statusline=-1',
+                     '-ao', 'alsa:device=bluealsa', url])
+
+
+#def play_gmusic_track_blocking(track_id, verbose=False):
+#    if api is None:
+#        init_gmusic_api()
+#
+#    t0 = time.time()
+#    print('play_gmusic_track_blocking')
+#    track_url = api.get_stream_url(track_id, device_id=gmusic_secrets.DEVICE_ID)
+#    print('play_gmusic_track_blocking got url: %0.2f' % (time.time() - t0))
+#
+#    if verbose:
+#        print(track_url)
+#
+#    resp = requests.get(track_url)
+#    resp.raise_for_status()
+#
+#    with tempfile.NamedTemporaryFile('wb') as f:
+#        f.write(resp.content)
+#        print('play_gmusic_track_blocking got content: %0.2f' % (time.time() - t0))
+#        play_mp3_file_blocking(f)
+
+
+def stream_gmusic_track_blocking(track_id, verbose=False):
     if api is None:
         init_gmusic_api()
 
+    t0 = time.time()
     print('play_gmusic_track_blocking')
     track_url = api.get_stream_url(track_id, device_id=gmusic_secrets.DEVICE_ID)
+    print('play_gmusic_track_blocking got url: %0.2f' % (time.time() - t0))
 
     if verbose:
         print(track_url)
 
-    resp = requests.get(track_url)
-    resp.raise_for_status()
-
-    with tempfile.NamedTemporaryFile('wb') as f:
-        f.write(resp.content)
-        play_mp3_file_blocking(f)
+    stream_mp3_url_blocking(track_url)
 
 
 def main():
+    print('hello gmusicplay')
     parser = argparse.ArgumentParser()
     parser.add_argument('track_id', action="store", type=str)
     parser.add_argument('-v', '--verbose', action="store_true", default=False)
     args = parser.parse_args()
 
-    play_gmusic_track_blocking(args.track_id)
+    stream_gmusic_track_blocking(args.track_id, verbose=args.verbose)
 
 
 if __name__ == '__main__':
